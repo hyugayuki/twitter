f34bbda50cc3793d3f00e05ee5c29b4d994e97ac
hyugayuki <hyugayuki@hyuugayuukinoMacBook-Pro.local>
2017-10-19 15:55:05 +0900

    Extend post form at main-wrapper

---
+++ app/assets/javascripts/tweets/post_area.js
@@ -0,0 +1,14 @@
+$(function(){
+  $("#post-input-fadeOut").click(function(){
+    $(this).css("display", "none");
+    $("#post-textarea").css("display", "");
+  });
+
+  $(document).on('click', function(e){
+    if (!$(e.target).closest('#post-textarea').length && !$(e.target).closest('#post-input-fadeOut').length){
+      console.log('test');
+      $("#post-input-fadeOut").css("display", "");
+      $("#post-textarea").css("display", "none");
+    };
+  });
+});
