df004640886bbcb527a770962ac9e366a4af586d
hyugayuki <hyugayuki@hyuugayuukinoMacBook-Pro.local>
2017-10-21 15:36:11 +0900

    Edit model to make validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :name, null: false
+  validates :user_identifier, null: false
 end
