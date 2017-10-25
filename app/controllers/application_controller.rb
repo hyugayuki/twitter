d9f4824221abae47da42d4cd3a7ca6462b68155c
hyugayuki <hyugayuki@hyuugayuukinoMacBook-Pro.local>
2017-10-25 16:59:12 +0900

    Set tweet instance to user post_modal

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
+  before_action :set_tweet
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_out_path_for(resource)
@@ -10,6 +11,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :user_identifier])
+  end
+
+  def set_tweet
+    @tweet = Tweet.new
   end
 
 end
