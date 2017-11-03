124de7e339c82520e3341677b406f8e3f774ec23
hyugayuki <hyugayuki@hyuugayuukinoMacBook-Pro.local>
2017-11-03 17:56:24 +0900

    Edit user and set link to edit_user_registration_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :user_identifier, :image])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :image])
   end
 
   def set_tweet
